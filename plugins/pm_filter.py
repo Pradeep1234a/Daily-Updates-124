ef79291e6f8c6dd5f26a9ae3092a2ce1eba1ea3f
Pradeep1234a <93538365+Pradeep1234a@users.noreply.github.com>
2022-01-14 06:24:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -427,6 +427,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton('🏡 Home', callback_data='start'),
             InlineKeyboardButton('📶 Status', callback_data='stats')
+            ],[
+            InlineKeyboardButton('🔆 Manual Filters', callback_data='manuelfilter'),
+            InlineKeyboardButton('🔅 Auto Filter', callback_data='autofilter')
+            ],[
+            InlineKeyboardButton('🔗 Connection', callback_data='coct'),
+            InlineKeyboardButton('🌀 Extra Mods', callback_data='extra')
+            ],[
+            InlineKeyboardButton('🏡 Home', callback_data='start'),
+            InlineKeyboardButton('📶 Status', callback_data='stats')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
